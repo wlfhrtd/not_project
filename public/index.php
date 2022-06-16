2dbea46307bdec93d519f30cc6c6750576df57f8
slrnd <strmbld@gmail.com>
2022-06-16 15:43:30 +0300

    procfile

--- public/index.php
+++ public/index.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+    $trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
+    if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+    if($trustedProxies) {
+        Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
+    }
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
